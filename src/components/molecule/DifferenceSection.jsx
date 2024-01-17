4ea85837f13e699f48d93dff957e3983d5d3cf07
abhiseknevrio <abhisek@nevrio.tech>
2024-01-17 12:28:43 +0530

    fix : ui inhancement

--- src/components/molecule/DifferenceSection.jsx
+++ src/components/molecule/DifferenceSection.jsx
@@ -1,36 +1,46 @@
 import React from 'react'
 
 const DifferenceSection = () => {
+
+    const data = [
+        {
+            id: "01",
+            icon: "/images/diff/frame.svg",
+            content: "Enhance Fitness, Achieve Vitality, Improve Performance, Boost"
+        },
+        {
+            id: "02",
+            icon: "/images/diff/alarm-star.svg",
+            content: "Energy, Improve Anxiety, Live Free Of Aches And Pains, Improve"
+        },
+        {
+            id: "03",
+            icon: "/images/diff/star-rating.svg",
+            content: "Sleep And Immunity."
+        },
+
+    ]
+
     return (
-        <section className='bg-diffrentSection mt-125px relative'>
+        <section className='bg-diffrentSection differenceSec relative'>
             <img className='absolute -top-100 right-right0' src='/images/whatsapp.svg' alt='' />
-            <div className='flex justify-center'>
-                <div className='mt-95px mb-106px'>
-                    <div className='flex justify-center'>
-                        <h1 className='text-55px font-bold text-center md:w-1032'>
-                            Feel the difference, just like 10,000+ of our customers
-                        </h1>
-                    </div>
-                    <div className='mt-60px grid grid-cols-1 md:grid-cols-3 gap-x-14 text-center font-semibold text-25px'>
-                        <div className='w-435'>
-                            <div className='flex justify-center mb-30px'>
-                                <img src='/images/diff/frame.svg' alt='' />
+            <div className=''>
+                <div className='flex justify-center'>
+                    <h1 className='text-55px font-bold text-center md:w-1032'>
+                        Feel the difference, just like 10,000+ of our customers
+                    </h1>
+                </div>
+                <div className='mt-60 grid grid-cols-1 md:grid-cols-3 gap-x-14 text-center font-semibold text-25px'>
+                    {
+                        data.map(item => (
+                            <div key={item.id} className='w-435'>
+                                <div className='flex justify-center'>
+                                    <img src={item.icon} alt='' />
+                                </div>
+                                <div className='mt-30'>{item.content}</div>
                             </div>
-                            <div>Enhance Fitness, Achieve Vitality, Improve Performance, Boost</div>
-                        </div>
-                        <div className='w-435'>
-                            <div className='flex justify-center mb-30px'>
-                                <img src='/images/diff/alarm-star.svg' alt='' />
-                            </div>
-                            <div>Energy, Improve Anxiety, Live Free Of Aches And Pains, Improve</div>
-                        </div>
-                        <div className='w-435'>
-                            <div className='flex justify-center mb-30px'>
-                                <img src='/images/diff/star-rating.svg' alt='' />
-                            </div>
-                            <div>Sleep And Immunity.</div>
-                        </div>
-                    </div>
+                        ))
+                    }
                 </div>
             </div>
         </section>
