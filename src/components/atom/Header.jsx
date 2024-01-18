c687134cfe649e1d5ffe3919d64eb8711e9f2c39
abhiseknevrio <abhisek@nevrio.tech>
2024-01-18 11:11:06 +0530

    feat : header done for mobile

--- src/components/atom/Header.jsx
+++ src/components/atom/Header.jsx
@@ -5,9 +5,11 @@ const Header = () => {
 
     return (
         <header className='border-b border-borderCol'>
-            <div className='mx-common flex justify-between items-center'>
-                <img src='/images/nutranourish-logo.png' className='h-16 my-7' alt='' />
-                <Button text="Take The Quiz" />
+            <div className='mx-common flex justify-center md:justify-between items-center'>
+                <img src='/images/nutranourish-logo.png' className='h-11 md:h-16 my-7' alt='' />
+                <div className='hidden md:block'>
+                    <Button text="Take The Quiz" />
+                </div>
             </div>
         </header>
     )
