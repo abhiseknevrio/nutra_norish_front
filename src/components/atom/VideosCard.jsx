18a2dfaf2493f17cc7365b9bc68d3ba46627170c
abhiseknevrio <abhisek@nevrio.tech>
2024-01-18 17:12:03 +0530

    feat : max BlogSection done

--- src/components/atom/VideosCard.jsx
+++ src/components/atom/VideosCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const VideosCard = ({ title, img, link }) => {
     return (
         <a href={link} target='_blank' rel="noreferrer">
-            <div className=''>
+            <div className='md:ml-10'>
                 <div className='flex justify-center'>
                     <img className='rounded-2xl' src={img} alt={title} />
                 </div>
