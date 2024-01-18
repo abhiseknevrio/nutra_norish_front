9f0b3c4636baeabae4aef5e20e2f32b762b3bf88
abhiseknevrio <abhisek@nevrio.tech>
2024-01-18 16:25:24 +0530

    feat  : PersonalisedSection for mobile

--- src/components/molecule/PersonalisedSection.jsx
+++ src/components/molecule/PersonalisedSection.jsx
@@ -8,23 +8,44 @@ const PersonalisedSection = () => {
         // backgroundSize: 'cover',
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
-        width: '100vw',
+        width: '100%',
         height: '763px',
     };
 
     return (
-        <section className='md:overflow-hidden'>
-            <div className='flex justify-center items-center' style={containerBg}>
-                <div className='personalisedSec'>
-                    <div className='flex flex-col md:flex-row items-center md:gap-20'>
-                        <div>
-                            <img src='/images/newworld/01.svg' alt='' />
+        <section>
+
+            {/* for desktop */}
+            <div className='hidden md:block'>
+                <div className='flex justify-center items-center' style={containerBg}>
+                    <div className='personalisedSec'>
+                        <div className='flex items-center gap-20'>
+                            <div>
+                                <img className='p-5' src='/images/newworld/01.svg' alt='' />
+                            </div>
+                            <div>
+                                <div className='text-55px font-bold'>Personalized Plans,Best Value</div>
+                                <div className='text-20px font-semibold mt-30'>We have worked hard to eliminate middlemen and overheads, so you get premium supplements at lowest prices.</div>
+                                <div className='text-20px font-semibold mt-30'>Get 10% off your first order + free shipping. You can cancel or change up your subscription any time.</div>
+                                <div className='mt-30'>
+                                    <Button text={"TAKE THE QUIZ"} />
+                                </div>
+                            </div>
                         </div>
-                        <div>
-                            <div className='text-55px font-bold'>Personalized Plans,Best Value</div>
-                            <div className='text-20px font-semibold mt-30'>We have worked hard to eliminate middlemen and overheads, so you get premium supplements at lowest prices.</div>
-                            <div className='text-20px font-semibold mt-30'>Get 10% off your first order + free shipping. You can cancel or change up your subscription any time.</div>
-                            <div className='mt-30'>
+                    </div>
+                </div>
+            </div>
+
+            {/* for mobile */}
+            <div className='md:hidden'>
+                <div className='flex justify-center items-center'>
+                    <div className='personalisedSec'>
+                        <img className='md:p-5' src='/images/newworld/01.svg' alt='' />
+                        <div className='mt-10'>
+                            <div className='text-30px text-center font-bold'>Personalized Plans,Best Value</div>
+                            <div className='text-20px text-center font-semibold mt-30'>We have worked hard to eliminate middlemen and overheads, so you get premium supplements at lowest prices.</div>
+                            <div className='text-20px text-center font-semibold mt-30'>Get 10% off your first order + free shipping. You can cancel or change up your subscription any time.</div>
+                            <div className='mt-30 flex justify-center'>
                                 <Button text={"TAKE THE QUIZ"} />
                             </div>
                         </div>
