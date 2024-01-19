7219dba5b1e37ca536018b6e908cd98144be181d
abhiseknevrio <abhisek@nevrio.tech>
2024-01-19 19:24:44 +0530

    feat : footer and Inhance Ui

--- src/components/atom/Input.jsx
+++ src/components/atom/Input.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Input = ({ label, placeholder, type }) => {
     return (
         <div>
-            <label className='text-20px font-semibold'>{label}</label>
+            <label className='text-20px font-semibold block'>{label}</label>
             <input placeholder={placeholder} type={type} className='input pl-5 py-7 mt-3' />
         </div>
     )
--- src/components/molecule/PersonalisedSection.jsx
+++ src/components/molecule/PersonalisedSection.jsx
@@ -12,7 +12,7 @@ const PersonalisedSection = () => {
                             <img className='p-5' src='/images/personalised/personalised.svg' alt='' />
                         </div>
                         <div className='personalised-right text-center lg:text-left'>
-                            <div className='text-55px font-bold'>Personalized Plans,Best Value</div>
+                            <div className='text-30px lg:text-55px font-bold'>Personalized Plans,Best Value</div>
                             <div className='text-20px font-semibold mt-30'>We have worked hard to eliminate middlemen and overheads, so you get premium supplements at lowest prices.</div>
                             <div className='text-20px font-semibold mt-30'>Get 10% off your first order + free shipping. You can cancel or change up your subscription any time.</div>
                             <div className='mt-30'>
