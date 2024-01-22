1851c4d3f50471a1f188633c1331e3164b42fbc2
abhiseknevrio <abhisek@nevrio.tech>
2024-01-22 16:50:31 +0530

    re-fac : new world sec done

--- src/components/atom/NewWorldCard.jsx
+++ src/components/atom/NewWorldCard.jsx
@@ -9,18 +9,19 @@ const NewWorldCard = ({ item, index }) => {
                     <div className=' flex justify-center items-center newWCard-left' style={{ background: item.bgLeftColor }}>
                         <div>
                             <img className={`${index === 3 ? "pt-5" : "rounded-full"}`} src={item.image} alt='' />
-                            {item.name &&
-                                <div className='text-30px font-bold text-center'>Dr Menka Gupta</div>
-                            }
-                            {item.designation &&
-                                <div className='text-30px font-semibold text-healthText text-center'>MD, IFMCP</div>
+                            {
+                                item.name && item.designation && <div className='mt-8'>
+                                    <div className='text-3xl font-bold text-center'>Dr Menka Gupta</div>
+
+                                    <div className='text-3xl font-semibold text-healthText text-center'>MD, IFMCP</div>
+                                </div>
                             }
                         </div>
                     </div>
-                    <div className='my-24 newWCard-right'>
-                        <div className='text-40px font-medium'>{item.id}</div>
-                        <div className='text-42px font-bold mt-30'>{item.title}</div>
-                        <div className='text-25px font-semibold mt-30'>{item.content}</div>
+                    <div className='my-12 md:my-24 newWCard-right'>
+                        <div className='title40 font-medium'>{item.id}</div>
+                        <div className='title42 mt-30'>{item.title}</div>
+                        <div className='text-2xl font-semibold mt-30'>{item.content}</div>
                     </div>
                 </div>
             </div>
