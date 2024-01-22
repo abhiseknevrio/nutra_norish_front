a64ca78b0cf8805525cef7a484c3967863e839fc
abhiseknevrio <abhisek@nevrio.tech>
2024-01-22 16:28:56 +0530

    re-fac : diff sec done

--- src/components/molecule/DifferenceSection.jsx
+++ src/components/molecule/DifferenceSection.jsx
@@ -26,11 +26,11 @@ const DifferenceSection = () => {
             <img className='absolute -top-100 right-right0 whatsIcon' src='/images/whatsapp.svg' alt='' />
             <div className=''>
                 <div className='flex justify-center'>
-                    <h1 className='text-30px md:text-55px font-bold text-center md:w-1032'>
+                    <h1 className='title55 text-center md:w-1032'>
                         Feel the difference, just like 10,000+ of our customers
                     </h1>
                 </div>
-                <div className='mt-60 grid grid-cols-1 md:grid-cols-3 gap-y-14 md:gap-x-14 text-center font-semibold text-25px'>
+                <div className='mt-60 grid grid-cols-1 md:grid-cols-3 gap-y-14 md:gap-x-14 text-center font-semibold text-xl md:text-2xl'>
                     {
                         data.map(item => (
                             <div key={item.id} className='w-435px'>
