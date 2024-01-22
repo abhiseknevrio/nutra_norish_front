7b82cdb936d9cb3976480620eb1a0448fbf71258
abhiseknevrio <abhisek@nevrio.tech>
2024-01-22 16:24:20 +0530

    re-fac : hero sec done

--- src/components/molecule/QuizCard.jsx
+++ src/components/molecule/QuizCard.jsx
@@ -4,8 +4,8 @@ import QuizInput from '../atom/QuizInput';
 const QuizCard = () => {
     return (
         <div className='w-1250 text-center p-20 quizBox'>
-            <div className='font-bold text-50px mb-9'>How old are you?</div>
-            <div className='mt-56px'>
+            <div className='title50'>How old are you?</div>
+            <div className='mt-9'>
                 <QuizInput />
             </div>
         </div>
