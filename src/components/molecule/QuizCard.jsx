196b8dfc354edc39d38c4eccc0251af2d4fda25e
abhiseknevrio <abhisek@nevrio.tech>
2024-01-23 15:52:10 +0530

    commit : working

--- src/components/molecule/QuizCard.jsx
+++ src/components/molecule/QuizCard.jsx
@@ -1,53 +1,78 @@
 import React, { useState } from 'react';
 
 const QuizCard = ({ questions }) => {
-
     const [userData, setUserData] = useState([]);
-    const [question, setNextQue] = useState(questions[0])
-    const [userInput, setUserInput] = useState(null)
+    const [question, setNextQue] = useState(questions[0]);
+    const [userInput, setUserInput] = useState(null);
 
-    console.log("userData : ", userData)
-    console.log("question : ", question)
+    // console.log("userData : ", userData);
+    // console.log("question : ", question);
 
     const addUserData = (key, val, next) => {
-        setUserData([...userData, { key: key, val: val }]);
-        nextQue(next)
-    }
+        setUserData((prevUserData) => [...prevUserData, { key: key, val: val }]);
+        nextQue(next);
+    };
 
     const nextQue = (val) => {
-        setNextQue(questions?.filter(item => item.key === val)[0])
-    }
+        const que = questions?.find((item) => item.key === val)
+        try {
+            if (que !== undefined) {
+                setNextQue(que)
+            }
+        } catch (error) {
+            console.error("nextQue error", error)
+        }
+    };
 
     return (
         <div className='text-center p-20 quizBox'>
             <div className='title50'>{question?.question}{question?.required && <span>*</span>}</div>
             <div className='mt-9'>
                 <div className='grid grid-cols-2 gap-x-10'>
-                    {(question?.type === "single_select" || question?.type === "multi_select") ?
-                        question?.options?.map(item => (
-                            <div key={item.key}>
-                                <div className='text-lg border border-borderGreen rounded-full py-2.5 px-5 inline-block'>
-                                    <input onClick={() => addUserData(item.key, item.value, item.nextQuestion)} type="radio" id={item.key} name="question" value={item.key} />
-                                    <label className='px-1.5' htmlFor={item.key}>{item.value}</label>
+                    {question.type ?
+                        <>
+                            {question?.options?.map((item) => (
+                                <div key={item.key}>
+                                    {(question.type === "single_select" || question?.type === "multi_select") &&
+                                        <div>
+                                            <div className='text-lg border border-borderGreen rounded-full py-2.5 px-5 inline-block'>
+                                                <input
+                                                    onClick={() => addUserData(item.key, item.value, item.nextQuestion)}
+                                                    type="radio"
+                                                    id={item.key}
+                                                    name="question"
+                                                    value={item.key}
+                                                />
+                                                <label className='px-1.5' htmlFor={item.key}>{item.value}</label>
+                                            </div>
+                                        </div>
+                                    }
+                                    {question.type === "input" &&
+                                        <div className='quizInputContainer'>
+                                            <input
+                                                onChange={(e) => setUserInput(e.target.value)}
+                                                className='quizInput rounded-full'
+                                                type='text'
+                                            />
+                                            <img
+                                                onClick={() => addUserData(item.key, userInput, item.nextQuestion)}
+                                                className='absolute cursor-pointer'
+                                                style={{ right: "40px" }}
+                                                src='/images/rightArrow.svg'
+                                                alt=''
+                                            />
+                                        </div>
+                                    }
                                 </div>
-                            </div>
-                        ))
+                            ))}
+                        </>
                         :
-                        question?.type === "input" ?
-                            question?.options?.map(item => (
-                                <div key={item.key} className='quizInputContainer'>
-                                    <input onChange={e => setUserInput(e.target.value)} className='quizInput rounded-full' type='text' />
-                                    <img onClick={addUserData(item.key, userInput, item.nextQuestion)} className='absolute cursor-pointer' style={{ right: "40px" }} src='/images/rightArrow.svg' alt='' />
-                                </div>
-                            ))
-
-                            :
-                            null
+                        null
                     }
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default QuizCard;
