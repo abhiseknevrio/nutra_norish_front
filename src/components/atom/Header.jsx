60f3f81bfbfb626bf92d372c5a4eeac148ef9af1
abhiseknevrio <abhisek@nevrio.tech>
2024-02-12 16:38:29 +0530

    feat : header logo link

--- src/components/atom/Header.jsx
+++ src/components/atom/Header.jsx
@@ -6,7 +6,9 @@ const Header = () => {
     return (
         <header id="quizsection" className='border-b border-borderCol'>
             <div className='mx-common flex justify-center lg:justify-between items-center'>
-                <img src='https://cdn.shopify.com/s/files/1/0606/0703/7648/files/nutranourish-logo-rr.svg' className='h-11 md:h-16 my-7' alt='' />
+                <a href='/'>
+                    <img src='https://cdn.shopify.com/s/files/1/0606/0703/7648/files/nutranourish-logo-rr.svg' className='h-11 md:h-16 my-7' alt='' />
+                </a>
                 <div className='hidden lg:block'>
                     <a href='#quizsection'>
                         <Button text="TAKE THE QUIZ" />
