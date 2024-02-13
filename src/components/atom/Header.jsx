4a0bbdbd62031d9da4d2440558514bfc4be92976
abhiseknevrio <abhisek@nevrio.tech>
2024-02-13 10:27:49 +0530

    feat : header scroll added but gllitch pending

--- src/components/atom/Header.jsx
+++ src/components/atom/Header.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Button from './Button'
 
 const Header = () => {
 
+    const [colorChange, setColorChange] = useState(false);
+    const changeNavbarColor = useCallback(() => {
+        if (window.scrollY >= 20) {
+            setColorChange(true);
+        } else {
+            setColorChange(false);
+        }
+    }, []);
+
+    useEffect(() => {
+        window.addEventListener('scroll', changeNavbarColor);
+        return () => {
+            window.removeEventListener('scroll', changeNavbarColor);
+        };
+    }, [changeNavbarColor]);
+
+    const backGround = colorChange ? "bg-cardBg fixed top-0 z-20" : "";
+
     return (
-        <header id="quizsection" className='border-b border-borderCol'>
+        <header id="quizsection" className={`min-w-full border-b border-borderCol ${backGround}`}>
             <div className='mx-common flex justify-center lg:justify-between items-center'>
                 <a href='/'>
                     <img src='https://cdn.shopify.com/s/files/1/0606/0703/7648/files/nutranourish-logo-rr.svg' className='h-11 md:h-16 my-7' alt='' />
