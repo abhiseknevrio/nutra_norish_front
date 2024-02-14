400353311ee3b1fdcba40aaf38bd04034acc223b
abhiseknevrio <abhisek@nevrio.tech>
2024-02-14 11:57:58 +0530

    fix : font family

--- src/components/molecule/DifferenceSection.jsx
+++ src/components/molecule/DifferenceSection.jsx
@@ -25,9 +25,9 @@ const DifferenceSection = () => {
         <section className='differenceSec'>
             <div className=''>
                 <div className='flex justify-center'>
-                    <h1 className='title55 text-center md:w-1032'>
+                    <div className='title55 text-center md:w-1032'>
                         Feel the difference, just like 10,000+ of our customers
-                    </h1>
+                    </div>
                 </div>
                 <div className='mt-60 grid grid-cols-1 md:grid-cols-3 gap-y-14 md:gap-x-14 text-center font-semibold text-xl md:text-2xl'>
                     {
