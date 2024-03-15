eb10c89e8b7df41942b9ff5079f6fcba0effb1ff
abhiseknevrio <abhisek@nevrio.tech>
2024-03-15 11:48:07 +0530

    fix : comment image on feedback

--- src/components/atom/ClientFeeedbackCard.jsx
+++ src/components/atom/ClientFeeedbackCard.jsx
@@ -7,7 +7,7 @@ const ClientFeeedbackCard = ({ item }) => {
             <div className='text-2xl font-bold'>{item.title}</div>
             <div className='text-base md:text-lg mt-30'>{item.content}</div>
             <div className='mt-30 flex justify-center items-center gap-5'>
-                <img loading='lazy' style={{ borderRadius: "46% 54% 62% 38% / 45% 32% 68% 55%" }} src={item.client.image} alt='' />
+                {/* <img loading='lazy' style={{ borderRadius: "46% 54% 62% 38% / 45% 32% 68% 55%" }} src={item.client.image} alt='' /> */}
                 <div>
                     <div className='text-base'>{item.client.name}</div>
                     <div>{item.client.linkText}</div>
